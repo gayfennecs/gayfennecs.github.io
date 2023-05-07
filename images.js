f24ba013bd76987638edfc1f0e8d2db54e6a5391
ExtraSome5 <extrasome5@gmail.com>
2023-05-07 03:03:54 -0300

    SONA SUBDOMAIN REAL!

--- images.js
+++ images.js
@@ -1,8 +1,20 @@
 window.onload = choosePic;
 
-var myPix = new Array("scrunkly_ch.png","scrunkly_mei.jpg","scrunkly_cr.jpg","scrunkly_anna.jpg","scrunkly_co2.jpg","scrunkly_cotton.jpg","scrunkly_fluff.png","scrunkly_dudettes.jpg","scrunkly_dudettes_2.jpg","scrunkly_skedgy.png");
+var scrunklys = [
+"scrunkly_anna.jpg",
+"scrunkly_ch.png",
+"scrunkly_co2.jpg",
+"scrunkly_cotton.jpg",
+"scrunkly_cr.jpg",
+"scrunkly_dudettes_2.jpg",
+"scrunkly_dudettes.jpg",
+"scrunkly_favicon.png",
+"scrunkly_fluff.png",
+"scrunkly_mei.jpg",
+"scrunkly_skedgy.png",
+]
 
 function choosePic() {
-     var randomNum = Math.floor(Math.random(10) * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
+     var randomNum = Math.floor(Math.random(10) * scrunklys.length);
+     document.getElementById("scrunklys").src = scrunklys[randomNum];
  }
